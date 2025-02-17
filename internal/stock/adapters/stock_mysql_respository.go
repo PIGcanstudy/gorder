516ce7a5a13c46c8d1a61e802126a976092b8029
PigCanStudy <1464451627@qq.com>
2025-02-17 15:06:44 +0800

    更新了悲观锁与乐观锁解决库存扣减竞争问题

--- internal/stock/adapters/stock_mysql_respository.go
+++ internal/stock/adapters/stock_mysql_respository.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type MySQLStockRepository struct {
@@ -56,29 +57,81 @@ func (m MySQLStockRepository) UpdateStock(
 				logrus.Warnf("update stock transaction err=%v", err)
 			}
 		}()
-		var dest []*persistent.StockModel
-		// 根据产品id列表获取对应库存数据列表
-		if err = tx.Table("o_stock").Where("product_id IN ?", getIDFromEntities(data)).Find(&dest).Error; err != nil {
-			return errors.Wrap(err, "failed to find data")
-		}
-		// 反序化数据库数据为[]*entity.ItemWithQuantity形式
-		existing := m.unmarshalFromDatabase(dest)
+		err = m.updatePessimistic(ctx, tx, data, updateFn)
+		//err = m.updateOptimistic(ctx, tx, data, updateFn)
+		return err
+	})
+}
 
-		// 执行更新数据逻辑
-		updated, err := updateFn(ctx, existing, data)
+// 乐观锁更新库存函数
+func (m MySQLStockRepository) updateOptimistic(
+	ctx context.Context,
+	tx *gorm.DB,
+	data []*entity.ItemWithQuantity,
+	updateFn func(ctx context.Context, existing []*entity.ItemWithQuantity, query []*entity.ItemWithQuantity,
+	) ([]*entity.ItemWithQuantity, error)) error {
+	var dest []*persistent.StockModel
+	if err := tx.Model(&persistent.StockModel{}).
+		Where("product_id IN (?)", getIDFromEntities(data)).
+		Find(&dest).Error; err != nil {
+		return errors.Wrap(err, "failed to find data")
+	}
 
-		if err != nil {
+	for _, queryData := range data {
+		var newestRecord persistent.StockModel
+		if err := tx.Model(&persistent.StockModel{}).Where("product_id = ?", queryData.ID).
+			First(&newestRecord).Error; err != nil {
 			return err
 		}
+		if err := tx.Model(&persistent.StockModel{}).
+			Where("product_id = ? AND version = ? AND quantity - ? >= 0", queryData.ID, newestRecord.Version, queryData.Quantity).
+			Updates(map[string]any{
+				"quantity": gorm.Expr("quantity - ?", queryData.Quantity),
+				"version":  newestRecord.Version + 1,
+			}).Error; err != nil {
+			return err
+		}
+	}
 
-		// 将数据更新到数据库中
-		for _, upd := range updated {
-			if err = tx.Table("o_stock").Where("product_id = ?", upd.ID).Update("quantity", upd.Quantity).Error; err != nil {
-				return errors.Wrapf(err, "unable to update %s", upd.ID)
+	return nil
+}
+
+// 悲观锁更新库存函数
+func (m MySQLStockRepository) updatePessimistic(
+	ctx context.Context,
+	tx *gorm.DB,
+	data []*entity.ItemWithQuantity,
+	updateFn func(ctx context.Context, existing []*entity.ItemWithQuantity, query []*entity.ItemWithQuantity,
+	) ([]*entity.ItemWithQuantity, error)) error {
+	var dest []*persistent.StockModel
+	// 找出与产品id对应的所有产品并加上forUpdate锁
+	if err := tx.Table("o_stock").
+		Clauses(clause.Locking{Strength: clause.LockingStrengthUpdate}).
+		Where("product_id IN (?)", getIDFromEntities(data)).
+		Find(&dest).Error; err != nil {
+
+		return errors.Wrap(err, "failed to find data")
+	}
+
+	existing := m.unmarshalFromDatabase(dest)
+	updated, err := updateFn(ctx, existing, data)
+	if err != nil {
+		return err
+	}
+
+	for _, upd := range updated {
+		for _, query := range data {
+			if upd.ID == query.ID {
+				// 执行更新逻辑
+				if err = tx.Table("o_stock").Where("product_id = ? AND quantity - ? >= 0", upd.ID, query.Quantity).
+					Update("quantity", gorm.Expr("quantity - ?", query.Quantity)).Error; err != nil {
+					return errors.Wrapf(err, "unable to update %s", upd.ID)
+				}
 			}
 		}
-		return nil
-	})
+
+	}
+	return nil
 }
 
 // 将库存数据反序化为[]*entity.ItemWithQuantity形式
--- internal/stock/go.mod
+++ internal/stock/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/PIGcanstudy/gorder/common v0.0.0-00010101000000-000000000000
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/viper v1.19.0
+	github.com/stretchr/testify v1.10.0
 	github.com/stripe/stripe-go/v79 v79.12.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.12
@@ -16,6 +17,7 @@ require (
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fatih/color v1.14.1 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
@@ -35,6 +37,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.59.0 // indirect
